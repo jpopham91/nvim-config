87e0472ac563221e6411e3e7185a5f20b8936f7a
jpopham91 <jxp6414@gmail.com>
2023-03-14 21:23:11 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require('plugins')
+require('remap.nvim')
+require('remap.telescope')
+
+vim.cmd.colorscheme('rose-pine')
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
