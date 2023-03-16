0792773b2ace04e960eb52e9faca2cf268e34168
jpopham91 <jxp6414@gmail.com>
2023-03-15 17:42:28 -0700

    hjkl colemak remap

--- lua/remap/nvim.lua
+++ lua/remap/nvim.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- More ergonomic nav for colemak
+vim.keymap.set('n', 'j', 'h') -- j = left
+vim.keymap.set('n', 'l', 'l') -- l = right
+vim.keymap.set('n', 'h', 'k') -- h = up
+vim.keymap.set('n', 'k', 'j') -- k = down
