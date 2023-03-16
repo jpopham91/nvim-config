dcf4a9f027fa0d16bb92f000f98bc9307bf40780
jpopham91 <jxp6414@gmail.com>
2023-03-15 19:43:51 -0700

    relative line numbers

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@ require('plugins')
 require('remap.nvim')
 require('remap.telescope')
 
+vim.cmd.set('nu', 'rnu')
+
 vim.cmd.colorscheme('PaperColor')
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
