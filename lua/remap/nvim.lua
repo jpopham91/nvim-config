044b629825ca5086d710cc4fcd70221a86ec553c
jpopham91 <jxp6414@gmail.com>
2023-03-23 20:30:58 -0700

    use hjkl remap in both visual and insert modes

--- lua/remap/nvim.lua
+++ lua/remap/nvim.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- More ergonomic nav for colemak
+vim.keymap.set('n', 'j', 'h') -- j = left
+vim.keymap.set('n', 'l', 'l') -- l = right
+vim.keymap.set('n', 'h', 'k') -- h = up
+vim.keymap.set('n', 'k', 'j') -- k = down
+
 vim.keymap.set('x', 'j', 'h') -- j = left
 vim.keymap.set('x', 'l', 'l') -- l = right
 vim.keymap.set('x', 'h', 'k') -- h = up
@@ -12,6 +17,11 @@ function no()
   print('STAHP!')
   return '<nop>'
 end
+
+vim.keymap.set('n', '<Up>', no)
+vim.keymap.set('n', '<Down>', no)
+vim.keymap.set('n', '<Left>', no)
+vim.keymap.set('n', '<Right>', no)
 
 vim.keymap.set('x', '<Up>', no)
 vim.keymap.set('x', '<Down>', no)
