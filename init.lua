5d670ecb953530ab1cdeadf677bddae66b4d623a
jpopham91 <jxp6414@gmail.com>
2023-04-01 15:37:10 -0700

    tab cfg

--- init.lua
+++ init.lua
@@ -9,3 +9,8 @@ vim.cmd.set('nu', 'rnu')
 vim.cmd.colorscheme('PaperColor')
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
+-- set tab 
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
